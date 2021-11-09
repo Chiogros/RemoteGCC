6cefa315aea4f9ec832dfb7544937964c8336cf8
ralsei38 <fontana.theo.me@gmail.com>
2021-11-09 16:20:42 +0100

    compile script comments

--- compile.sh
+++ compile.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
-# looks for .c files in /mnt/in, compile it, put it on /mnt/out
+### looks for make & .c files in /mnt/in, compile them, put them on /mnt/out ###
+### compilation trigger => existence of ok.gcc file in /mnt/in ###
+
+
 while true; do
 
-    if [ -f ok.gcc]
-
-
-        #if there is a makefile 
-        if [ -f make ] then
-            #process it
+    if [ -f ok.gcc ]
+        #if there is a makefile process it
+        if [ -f make ]; then
             make
-        #else if there is one or more c files
-        elif [ -f *.c ] then
+
+        #else if there is C files compile them
+        elif [ -f *.c ]; then
             for $c_file in *.c; do
                 $name = $(basename $c_file .c)
                 gcc $c_file -o $name
             done
-        #else ?!
+
+        #if there is no makefile AND no c file(s), but the ok.gcc file exists
         else
-            echo "eh?"
+            echo "no makefile and no C file in this directory."
+            rm *
         fi
-        #RM or MV
         rm *.c
         rm make
+        #clear at some point ?
         mv * /mnt/out
     fi
 sleep 5

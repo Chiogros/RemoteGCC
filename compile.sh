13fec5d66f6f72c297654d671e079728eb02d0bf
ralsei38 <fontana.theo.me@gmail.com>
2021-11-10 15:01:17 +0100

    mv fix

--- compile.sh
+++ compile.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
+
 ### looks for make & .c files in /mnt/in, compile them, put them on /mnt/out ###
 ### compilation trigger => existence of ok.gcc file in /mnt/in ###
 
-
 while true; do
 
-    if [ -f ok.gcc ]
+    if [ -f ok.gcc ]; then
         #if there is a makefile process it
         if [ -f make ]; then
             make
 
         #else if there is C files compile them
         elif [ -f *.c ]; then
-            for $c_file in *.c; do
-                $name = $(basename $c_file .c)
+            for c_file in *.c; do
+                echo "$c_file"
+                name=$(basename $c_file .c)
                 gcc $c_file -o $name
+                echo "c compiled"
             done
 
         #if there is no makefile AND no c file(s), but the ok.gcc file exists
@@ -22,10 +24,13 @@ while true; do
             echo "no makefile and no C file in this directory."
             rm *
         fi
+
+        #CLEAR IN FOLDER
         rm *.c
-        rm make
-        #clear at some point ?
+        rm Makefile
+
+        #MV COMPILED CODE TO OUT FOLDER
         mv * /mnt/out
     fi
 sleep 5
-done;
+done

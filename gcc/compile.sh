50abe66f5b60a7265b6b954f2678b260775dd0da
Chiogros <alexandre.bouijoux@gmail.com>
2021-11-11 17:39:16 +0100

    Script v2 for integration with first container and bugs solved

--- gcc/compile.sh
+++ gcc/compile.sh
@@ -1,38 +1,69 @@
 #!/bin/bash
 
+inFolder=/mnt/in/
+outFolder=/mnt/out/
+logFolder=/mnt/log/
+
+compilationLogFile="${logFolder}compilation.log"
+tmpCompilationLogFile=/tmp/compilation.log
+
+triggerFile="ok.gcc"
+
 ### looks for make & .c files in /mnt/in, compile them, put them on /mnt/out ###
 ### compilation trigger => existence of ok.gcc file in /mnt/in ###
-cd /mnt/in
+cd $inFolder
+
 while true; do
 
-    if [ -f ok.gcc ]; then
-
-        #if there is a makefile process it
-        if [ -f Makefile ]; then
-            make
-        #else if there is C files compile them
-        elif [ -f *.c ]; then
-            for c_file in *.c; do
-                echo "$c_file"
-                name=$(basename $c_file .c)
-                gcc $c_file -o $name
-                echo "c compiled"
-            done
-
-        #if there is no makefile AND no c file(s), but the ok.gcc file exists
-        else
-            echo "no makefile and no C file in this directory."
-            rm *
-        fi
-
-        #CLEAR IN FOLDER
-        rm *.c
-        rm Makefile
-        rm ok.gcc
-
-        #MV COMPILED CODE TO OUT FOLDER
-        rm  -f /mnt/out/*
-        mv * /mnt/out/
-    fi
-sleep 5
+	if [ -f "$triggerFile" ]; then
+		
+		# Clear flag file
+		rm -f "$triggerFile" 2> /dev/null
+		
+		# Clear last log files
+		rm "$compilationLogFile" 2> /dev/null
+		rm "$tmpCompilationLogFile" 2> /dev/null
+		
+		# Clean old binaries
+		binaries="${outFolder}*"
+		rm -f $binaries
+		
+		# if there is a makefile, process it
+		if [ -f Makefile ]; then
+			make
+		
+		# else if there are C files, compile them
+		elif [ $(ls -1q *.c | wc -l) -ne 0 ]; then
+			for c_file in *.c; do
+				echo "Compiling ${c_file}..."
+				
+				# Create out binary pathname
+				name=$(basename "$c_file" .c)
+				outPath="${outFolder}${name}"
+				
+				# Compile to out folder and log
+				gcc "$c_file" -o "$outPath" &>> "$tmpCompilationLogFile"
+				
+				echo "${c_file} compiled!"
+			done
+			
+			# Write success compilation message in log
+			if [ ! -s "$tmpCompilationLogFile" ]; then
+				echo "Compilation went successful!" > "$tmpCompilationLogFile"
+			fi
+		
+		#if there is no makefile AND no C file(s), but the ok.gcc file exists
+		else
+			# Need to stop webserver loop
+			echo "Nothing to compile..." > "$tmpCompilationLogFile"
+		fi
+		
+		# clear input folder
+		rm * 2> /dev/null
+		
+		# move tmp log file to definite folder
+		mv "$tmpCompilationLogFile" "$compilationLogFile"
+		
+	 fi
+	 sleep 5
 done

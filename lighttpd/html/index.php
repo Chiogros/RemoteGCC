2f1aaa5686a405206dba311b5f63badae86b9a1f
Chiogros <alexandre.bouijoux@gmail.com>
2021-11-11 12:03:58 +0100

    Ignore . and .. files

--- lighttpd/html/index.php
+++ lighttpd/html/index.php
@@ -9,63 +9,66 @@ $logFile = "/var/log/apache2/access.log";
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-	 <title>MI4 - RemoteGCC</title>
-	 <meta charset="UTF-8">
-	 <link rel="stylesheet" href="style.css">
+	<title>🐋 MI4 - RemoteGCC</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="style.css">
 </head>
 <body>
-	 <header>
-			<h1>RemoteGCC</h1>
-	 </header>
-	 <section>
-			<article>
-				 <h2>Inputs</h2>
-				 <article>
-						<h3>Files</h3>
-						<?php
-						$dir = opendir($inputFolder);
-						while ($filename = readdir($dir)) { ?>
-							 <p><?= $filename ?></p> 			 
-						<?php } ?>
-				 </article>				 
-			</article>
-				 <form method="POST" action="">
-						<input type="file" accept="Makefile,.c,.h" multiple />
-						<input type="submit" value="Push"/>
-				 </form>
-			</article>
-			<article>
-				 <h2>Logs</h2>
-				 <p>
-				 <?php
-						# Open file
-						$f = fopen($logFile, "r");
-						if (!$f) {
-							 printf("Log file not available.");
-						} else {
-							 $content = "";
-							 # Read log content
-							 while (!feof($f)) {
-									$content .= fread($f, 1024);
-							 }
+	<header>
+		<h1>RemoteGCC</h1>
+	</header>
+	<section>
+		<article>
+			<h2>Inputs</h2>
+			<div class="filesList">
+				<?php
+				$dir = opendir($inputFolder);
+				while ($filename = readdir($dir)) { 
+					if ($filename == "." || $filename == "..") continue; ?>
+					<p><?= $filename ?></p>
+					<hr/>
+				<?php } ?>
+			</div>
+			<form method="POST" action="">
+				<input type="file" accept="Makefile,.c,.h" multiple />
+				<input type="submit" value="Push"/>
+			</form> 
+		</article>
+		<article>
+			<h2>Logs</h2>
+			<p id="log">
+			<?php
+			# Open file
+			$f = fopen($logFile, "r");
+			if (!$f) {
+				 printf("Log file not available.");
+			} else {
+				 $content = "";
+				 # Read log content
+				 while (!feof($f)) {
+						$content .= fread($f, 1024);
+				 }
 
-							 # Print log file
-							 print($content);
+				 # Print log file
+				 print($content);
 
-							 fclose($f);
-						}
-
-				 ?>
-				 </p>
-			</article>
-			<article>
-				 <h2>Binaries</h2>
-						<?php
-						$dir = opendir($outputFolder);
-						while ($filename = readdir($dir)) { ?>
-							 <p><?= $filename ?></p> 			 
-						<?php } ?>
-			</article>
-	 </section>
+				 fclose($f);
+			}
+			?>
+			</p>
+		</article>
+		<article>
+			<h2>Binaries</h2>
+			<div class="filesList">
+				<?php
+				$dir = opendir($outputFolder);
+				while ($filename = readdir($dir)) { 
+					if ($filename == "." || $filename == "..") continue; ?>
+					<p><?= $filename ?></p>
+					<hr/>
+				<?php } ?>
+			</div>
+		</article>
+	</section>
 </body>
 </html>
